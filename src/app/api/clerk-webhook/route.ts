0faacce5126109d30f1f97c09b6357c46c6cfb78
rayan <rayanhassan200691@gmail.com>
2025-09-20 12:30:49 +0500

    add clerk webhooks

--- src/app/api/clerk-webhook/route.ts
+++ src/app/api/clerk-webhook/route.ts
@@ -3,8 +3,10 @@ import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 import UserModel from "@/models/User";
+import dbConnect from "@/lib/dbConnect";
 
 export async function POST(req: NextRequest) {
+  dbConnect();
   const CLERK_WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
 
   if (!CLERK_WEBHOOK_SECRET) {
@@ -33,34 +35,33 @@ export async function POST(req: NextRequest) {
       "svix-timestamp": svix_timestamp,
       "svix-signature": svix_signature,
     }) as WebhookEvent;
-  
   } catch (error) {
     console.error("Error verifying webhook", error);
-    return new NextResponse("Error occured", {status: 400});
+    return new NextResponse("Error occured", { status: 400 });
   }
 
   const eventType = evt.type;
 
   if (eventType === "user.created") {
     try {
-        const {id, first_name, last_name, image_url, email_addresses} = evt.data;
-        const email = email_addresses[0].email_address;
-        const name = `${first_name || ""} ${last_name || ""}`.trim();
+      const { id, first_name, last_name, image_url, email_addresses } =
+        evt.data;
+      const email = email_addresses[0].email_address;
+      const name = `${first_name || ""} ${last_name || ""}`.trim();
 
-        const newUser = new UserModel({
-            email,
-            name,
-            image: image_url,
-            clerkId: id
-        });
-        await newUser.save();
+      const newUser = new UserModel({
+        email,
+        name,
+        image: image_url,
+        clerkId: id,
+      });
+      await newUser.save();
 
-        console.log("User created successfully");
-        return new NextResponse("User created!", {status: 200});
-        
+      console.log("User created successfully");
+      return new NextResponse("User created!", { status: 200 });
     } catch (error) {
-        console.log("Error creating user: ", error);
-        return new NextResponse("Error creating user", {status: 500});
+      console.log("Error creating user: ", error);
+      return new NextResponse("Error creating user", { status: 500 });
     }
   }
 }
